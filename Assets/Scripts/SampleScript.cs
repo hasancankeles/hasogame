f4b7edd25223cc76b69790857c9cb8534d7467f0
hasancankeles <keleshasancan@gmail.com>
2026-02-22 22:04:28 +0300

    Step 2.5: Created SampleScript deriving from MonoBehaviour and attached to MovableA

---
+++ Assets/Scripts/SampleScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("SampleScript has started.");
+    }
+
+    void Update()
+    {
+
+    }
+}
